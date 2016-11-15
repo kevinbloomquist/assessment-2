e90b82333bd110ca436a6fe2bb8efb772b48bcac
kevinbloomquist <rkevinbloom@gmail.com>
2016-11-15 13:53:05 -0700

    JS start adds get variables

---
+++ script.js
@@ -0,0 +1,27 @@
+
+var test = function() {
+console.log("new refresh!");
+};
+
+test();
+
+
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+
+
+var getElle = document.getElementById("elle");
+console.log(getElle);
+
+var getdrBrenner = document.getElementById("drBrenner");
+console.log(getdrBrenner);
+
+var getwill = document.getElementById("will");
+console.log(getwill);
+
+
+
+
